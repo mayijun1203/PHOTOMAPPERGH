5dc7320b6ee20037e052701fc2ba91a2926674e2
Yijun Ma <mayijun1203@gmail.com>
2023-01-31 00:22:14 +0800

    update

--- photoviz.py
+++ photoviz.py
@@ -3,18 +3,24 @@ import geopandas as gpd
 import shapely
 import exif
 import os
+import PIL
 
 # path=os.gecwd()
 # path='C:/Users/MaY8/Desktop/GITHUB/PHOTOVIZGH/'
 path='C:/Users/mayij/Desktop/DOC/GITHUB/PHOTOVIZGH/'
 pd.options.display.max_columns=100
 
-# imgpath=path+'img/IMG_0333.JPG'
+
+
+# EXIF list
+# imgpath=path+'photo/IMG_0332.JPG'
 # with open(imgpath,'rb') as src:
 #     img=exif.Image(src)
 # img.get_all()
 
 
+
+# Extract info
 def decimalcoords(orgcoords,ref):
     decimaldegrees=orgcoords[0]+orgcoords[1]/60+orgcoords[2]/3600
     if ref=='S' or ref=='W':
@@ -42,11 +48,18 @@ def imgcoords(imgpath):
                      'bearing':[img.gps_dest_bearing]})
     return(tp)
 
-
-
 df=[]
 for i in os.listdir(path+'original'):
     df+=[imgcoords(i)]
 df=pd.concat(df,axis=0)
 df=gpd.GeoDataFrame(df,geometry=[shapely.geometry.Point(xy) for xy in zip(df['long'],df['lat'])],crs=4326)
 df.to_file(path+'photoattr.geojson',crs=4326, driver='GeoJSON')
+
+
+
+# Compress photos
+for i in os.listdir(path+'original'):
+    tp=PIL.Image.open(path+'original/'+i)
+    tp.save(path+'photo/'+i,optimize=True,quality=30)
+    
+    
